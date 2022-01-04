a3ada13f2c1faf8acce45999a2e950dbecccffee
Marcelo Pereira <marcelo.fatecpoa@gmail.com>
2022-01-04 08:14:50 -0300

    Created repository

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Header() {
+  return (
+    <header className={styles.postHeader}>
+      <Link href="/">
+        <a>
+          <img src="/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </header>
+  );
+}
